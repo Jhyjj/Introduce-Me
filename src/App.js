f973232372fb4bc68e1d07ce13a4dbdcb8442290
jhyjj <jhyj3768@gmail.com>
2022-09-15 15:05:45 +0900

    모바일 반응형 수정중

--- src/App.js
+++ src/App.js
@@ -18,8 +18,53 @@ function App() {
             <span>아보카톡</span>
           </div>
       </div>
+      <div id="myphone">
+        {/* 헤더 */}
+          <div id="phone_header">
+            <div>시간</div>
+            <ul>
+              <li>상태바</li>
+              <li>와이파이</li>
+              <li>배터리</li>
+            </ul>
+          </div>
+
+          {/* 메인화면 */}
+          <div id="phone_main">
+            <div>
+              <span>캘린더</span>
+            </div>
+            <div>
+              <span>시계</span>
+            </div>
+            <div>
+              <span>카메라</span>
+            </div>
+            <div>
+              <span>날씨</span>
+            </div>
+          </div>
+
+          {/* 하단 */}
+          <div>
+          <div>
+              <span>전화</span>
+            </div>
+            <div>
+              <span>문자</span>
+            </div>
+            <div>
+              <span>아보카톡</span>
+            </div>
+            <div>
+              <span>설정</span>
+            </div>
+            
+          </div>
+
+      </div>
       <Intro/>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
